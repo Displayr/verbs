28a3304e7403696e5ce3f0a0bcb8b5cc6241266b
Tim Bock <tim.bock@numbers.net.au>
2020-10-30 11:19:37 +1100

    DS-3136 Unit tests and scaffolding

---
+++ R/sumrows.R
@@ -0,0 +1,9 @@
+#' @export
+SumRows <- function(...,
+                    remove.missing = TRUE,
+                    remove.rows = c("NET", "SUM", "Total"),
+                    remove.columns = c("NET", "SUM", "Total"),
+                    warn = FALSE)
+{
+    1
+}
