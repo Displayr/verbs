9ee0e34e8ddafc5d4fe697b91a89a014c865d439
Justin Yap <justinyapcc@gmail.com>
2020-11-19 17:02:05 +1100

    DS-3138: copy statistic attribute

--- tests/testthat/test-firstlast.R
+++ tests/testthat/test-firstlast.R
@@ -541,3 +541,45 @@ test_that("Row and Column unit", {
     expect_equal(First(arr, 1, "Column"), head(arr, c(NA, 1)))
     expect_equal(Last(arr, 1, "Column"), tail(arr, c(NA, 1)))
 })
+
+test_that("Statistic attribute is copied", {
+    q.table.with.statistic <- structure(c(25.0764525993884, 25.6880733944954, 27.3846153846154,
+                23.9263803680982, 27.6923076923077, 19.9386503067485, 0, 0.305810397553517,
+                0.307692307692308, 0.613496932515337, 0, 0, 0, 0, 0, 0, 0, 0.920245398773006,
+                28.7461773700306, 22.0183486238532, 22.4615384615385, 22.6993865030675,
+                15.0769230769231, 23.6196319018405, 7.03363914373089, 19.8776758409786,
+                15.3846153846154, 16.5644171779141, 23.6923076923077, 17.7914110429448,
+                14.3730886850153, 20.4892966360856, 20.9230769230769, 19.3251533742331,
+                23.6923076923077, 17.7914110429448, 5.81039755351682, 4.28134556574923,
+                2.76923076923077, 2.45398773006135, 3.38461538461538, 3.68098159509202,
+                4.28134556574923, 1.8348623853211, 1.53846153846154, 2.14723926380368,
+                2.46153846153846, 0.920245398773006, 5.19877675840979, 2.75229357798165,
+                4, 5.52147239263804, 2.15384615384615, 8.58895705521472, 2.75229357798165,
+                1.52905198776758, 2.46153846153846, 4.29447852760736, 0.615384615384615,
+                3.37423312883436, 3.05810397553517, 0.917431192660551, 2.15384615384615,
+                2.14723926380368, 0.923076923076923, 3.37423312883436, 3.6697247706422,
+                0.305810397553517, 0.615384615384615, 0.306748466257669, 0.307692307692308,
+                0, 100, 100, 100, 100, 100, 100), statistic = "Row %",
+              .Dim = c(6L, 13L),
+              .Dimnames = list(c("Coke", "Diet Coke", "Coke Zero", "Pepsi", "Diet Pepsi", "Pepsi Max"),
+                               c("Functional", "Price: Expensive", "Price: Inexpensive",
+                                 "Evaluative: Positive", "Evaluative: Negative",
+                                 "Other", "Reliable", "Fun", "Energising", "Sexy", "Strong", "Classic", "NET")),
+              basedescriptiontext = "base n = from 325 to 327; total n = 327; 2 missing",
+              basedescription = list(Minimum = 325L, Maximum = 327L, Range = TRUE, Total = 327L,
+                                     Missing = 2L, EffectiveSampleSize = 327L,
+                                     EffectiveSampleSizeProportion = 100, FilteredProportion = 0),
+              questiontypes = "PickOneMulti",
+              span = list(rows = structure(list(c("Coke", "Diet Coke", "Coke Zero", "Pepsi", "Diet Pepsi", "Pepsi Max")),
+                                           class = "data.frame", .Names = "", row.names = c(NA, 6L)),
+                          columns = structure(list(c("Functional", "Price: Expensive", "Price: Inexpensive",
+                                                     "Evaluative: Positive", "Evaluative: Negative",
+                                                     "Other", "Reliable", "Fun", "Energising", "Sexy", "Strong", "Classic", "NET")),
+                                              class = "data.frame", .Names = "", row.names = c(NA, 13L))),
+              name = "Q1.  Fragments coded", questions = c("Q1.  Fragments coded", "SUMMARY"))
+
+    result <- First(q.table.with.statistic)
+    expect_equal(attr(result, "statistic"), "Row %")
+    result <- Last(q.table.with.statistic)
+    expect_equal(attr(result, "statistic"), "Row %")
+})
