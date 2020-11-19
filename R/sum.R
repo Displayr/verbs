699c85ba51ed1f4885a6f500972995586468fc04
jrwishart <justin.wishart@displayr.com>
2020-11-19 11:59:39 +1100

    DS-3136 Create common Rd file for Sum and SumRows

--- R/sum.R
+++ R/sum.R
@@ -1,23 +1,51 @@
-#' General-Purpose Element-wise Summation
+#' @rdname SumOperations
+#' @title General-Purpose Summation functions
 #'
 #' A replacement for \code{\link{sum}} supporting element-wise
 #' summation of many objects in addition to vectors.
-#' @param ... Objects to be summed; e.g. Q Tables, arrays
-#' @param remove.missing Should missing values be removed in calculations? Defaults to \code{TRUE},
-#' @param remove.rows The categories of named vectors, or, rows of tables and similar
-#' structures to be removed.
-#' @param remove.columns The labels of categories to be removed from the calculation
-#' where the table is two-dimensional,
-#' @param warn Warnings are shown when non-obvious operations are performed (e.g.,
-#' removing rows).
+#' @param ... Objects to be summed; e.g. Q Tables, arrays, vectors, Variables or Variable Sets.
+#'   Depending on if \code{Sum}, \code{SumRows} or \code{SumColumns} is used, different possible
+#'   multiple input types are allowed. See the
+#' @param remove.missing Logical element controlling whether missing values should be
+#'   removed in calculations? Defaults to \code{TRUE}. If set to \code{FALSE} and one of
+#'   input elements contains missing values, then the resulting computed sum will also be
+#'   \code{NA}.
+#' @param remove.rows The labels of named vectors, or, rows of tables and similar
+#'   structures to be removed from the row dimension of the input. Any row elements
+#'   with the labels specified here will not be included in the resulting sum calculation.
+#' @param remove.columns The labels of columns of tables and similar
+#'   structures to be removed from the column dimension of the input. Any column elements
+#'   with the labels specified here will not be included in the resulting sum calculation.
+#' @param match.elements String element specifying if inputs with named row elements
+#'   (or named elements for a single vector) should be matched when used in \code{SumRows}.
+#' @param warn Logical element to control whther warnings are shown when non-obvious
+#'   operations are performed (e.g., removing rows, removing missing values when they are present).
 #' @param subset Logical vector of units to use in the calculation. Only applied to variables and
-#' not to QTables that contain statistics since the original variable data is unavailable.
+#'   not to QTables that contain statistics since the original variable data is unavailable.
 #' @param weights Numeric vector of weights to use in the calculation. Only applies to variables
-#' and not to QTables that contain statistics since the original variable data is unavailable.
-#' @return The sum of the provided inputs
+#'   and not to QTables that contain statistics since the original variable data is unavailable.
+#' @return The sum or sum of the rows or sum of the columns of the provided inputs respectively
+#'   depending on the function used.
 #' @export
 #' @examples
+#' # Examples using Sum
 #' Sum(1:3, 2:4)
+#' all.equal(Sum(1:3, 2:4), sum(1:3, 2:4))
+#' x <- 1:4
+#' names(x) <- c(LETTERS[1:3], "SUM")
+#' Sum(x)
+#' x <- 1:9
+#' desired.subset = rep(c(TRUE, FALSE), c(3, 6))
+#' Sum(x, subset = desired.subset)
+#' desired.weights <- runif(9)
+#' y <- 10:18
+#' Sum(x, y, weights = desired.weights)
+#' sum(x * desired.weights, y * desired.weights)
+#' basic.table <- matrix(1:12, nrow = 4, dimnames = list(letters[1:4], LETTERS[1:3]))
+#' table.with.marginals <- rbind(basic.table, SumColumns(basic.table))
+#' table.with.marginals <- cbind(table.with.marginals, c(SumRows(basic.table), Sum(basic.table)))
+#' dimnames(table.with.marginals) <- list(c(letters[1:4], "SUM"), c(LETTERS[1:3], "SUM"))
+#' all.equal(Sum(table.with.marginals), sum(basic.table))
 Sum <- function(...,
                 remove.missing = TRUE,
                 remove.rows = c("NET", "SUM", "Total"),
@@ -50,10 +78,10 @@ Sum <- function(...,
     sum.output
 }
 
-
 # extra arguments cannot be specified in Reduce
 sumWithNAsRemoved <- function(...)
     sum(..., na.rm = TRUE)
+
 
 sumElements <- function(x, sum.function)
 {
@@ -62,6 +90,3 @@ sumElements <- function(x, sum.function)
     else
         sum.function(x[[1]])
 }
-
-
-
