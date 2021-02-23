f2d08bc163ac966abbf208518dd61772b634250d
Mathew W. McLean <mathew.w.mcLean@gmail.com>
2021-02-23 15:33:06 +1100

    Add fun. to select from a table; DS-3171, DS-3190

--- R/select.R
+++ R/select.R
@@ -1,60 +1,174 @@
-#' @noRd
-#' Internal functions to select from QTables using Combo Box
-#' R Controls, which are reprene
-#' @return the number of tables supplied by the user using Table - Select GUI controls
-nTables <- function()
-    length(ls(pattern = "^formTables[0-9]+$", envir = .GlobalEnv)) - 1
-
-# Subsets a table based on values of combo.boxes
-selectFromTableUsingComboBoxes <- function(table, table.orig, combo.boxes, dim = 1)
+#' Select from row and columns of tables
+#'
+#' Subsets tables supporting a number of different modes for
+#' specifying the rows and columns to retain. In addition to the usual
+#' integer and character vector subsetting similar to \code{[}, first
+#' and last rows and columns can be retained similar to
+#' \code{\link{head}} and \code{\link{tail}}, and ranges of indices or
+#' dates can be specified as a character string. Special support for
+#' selecting from tables with dates in the rows and columns is also
+#' provided.
+#' @param table A table (\code{matrix} or \code{array}) to select
+#'     from.
+#' @param row.selection.mode String specifying how to select rows to
+#'     include in the output table. The default "vector"
+#' @param unit See \code{\link{First}}, only used when selection mode
+#'     involves. Either a scalar or length-2 vector to use different
+#'     units in rows and columns.
+#' @param calendar Only used when selection mode involves dates. A
+#'     logical scalar that indicates whether to consider calendar
+#'     periods when \code{unit} is a unit of time. Can also be
+#'     length-2 vector to use different options in rows and
+#'     columns. See Details in \code{\link{First}}.
+#' @param ... Additional arguments passed to \code{\link{First}}.
+#' @return A matrix containing the selected rows and columns of the
+#'     table specified by \code{table.to.show}.
+#' @importFrom flipTime AsDate AsDateTime
+SelectFromTable <- function(
+                             table,
+                             row.selection.mode = c("vector",
+                                                    "first rows",
+                                                    "last rows",
+                                                    "range",
+                                                    "first date-time periods",
+                                                    "last date-time periods",
+                                                    "date range"),
+                             row.selections = NULL,
+                             column.selection.mode = c("vector",
+                                                    "first columns",
+                                                    "last columns",
+                                                    "range",
+                                                    "first date-time periods",
+                                                    "last date-time periods",
+                                                    "date range"),
+                             column.selections = NULL,
+                             unit = NULL,
+                             calendar = TRUE,
+                             ...)
 {
-    stopifnot(dim == 1 || dim == 2)
+    row.selection.mode <- tolower(row.selection.mode)
+    column.selection.mode <- tolower(column.selection.mode)
+    row.selection.mode <- match.arg(row.selection.mode)
+    column.selection.mode <- match.arg(column.selection.mode)
 
-    dim.names <- dimnames(table)[[dim]]
-    idx <- which(combo.boxes)
-    if (dim == 1L){
-        table <- selectFromRows(table, table.orig, idx)
-    }else if(dim == 2L)
-        table <- selectFromColumns(table, table.orig, idx)
-    return(table)
-}
-
-selectFromRows <- function (table, table.orig, idx)
-{
-    n.dims <- length(dim(table))
-
-    if (is.character(idx))
-        idx <- checkSuppliedIndices(table, idx, 1)
-
-    if (n.dims == 1){
-        table <- table[idx, drop = FALSE]
-    }else if (n.dims == 2){
-        table <- table[idx, , drop = FALSE]
-    }else if (n.dims == 3){
-        table <- table[idx, , , drop = FALSE]
+    table.out <- FlattenTableAndDropStatisticsIfNecessary(table)
+    if (!is.null(row.selections))
+    {
+        row.selection.mode <- tolower(row.selection.mode)
+        table.out <- selectFromRows(table.out, row.selection.mode,
+                                    row.selections, unit = unit[1],
+                                    calendar = calendar[1], ...)
     }
 
-    if (hasRowSpan(table.orig))
-        table <- updateTableRowSpanAttribute(table, table.orig, idx)
-    table <- flipU::CopyAttributes(table, table.orig)
-    return(table)
+
+    if (NCOL(table.out) > 1L && !is.null(column.selections))
+    {
+        if (length(calendar) == 2)
+            calendar <- calendar[2]
+        if (length(unit) == 2)
+            unit <- unit[2]
+        table.out <- selectFromColumns(table.out, table.out,
+                                       selection.mode = column.selection.mode,
+                                       selections = column.selections,
+                                       unit = unit, calendar = calendar, ...)
+
+    }
+
+
+    if (!is.null(attr(table.out, "name")))
+    {
+        attr(table.out, "name.original") <- attr(table.out, "name")
+        attr(table.out, "name") <- NULL
+    }
+    return(table.out)
 }
 
-selectFromColumns <- function(table, table.orig, idx)
+selectFromRows <- function(table, selection.mode = "vector",
+                           selections = NULL, unit, calendar, ...)
 {
+    table.out <- table
+    if(selection.mode == "range")
+        selections <- parseRangeString(selections)
+    else if (selection.mode == "date range")
+        selections <- findDatesInTable(table, selections, 1)
+    else if (selection.mode == "first rows")
+        unit <- "Row"
+
     n.dims <- length(dim(table))
 
-    if (is.character(idx))
-        idx <- checkSuppliedIndices(table, idx, 2)
-    if (length(dim(table)) == 2){
-        table <- table[, idx, drop = FALSE]
-    }else if (length(dim(table)) == 3)
-        table <- table[, idx, , drop = FALSE]
+    if (grepl("^first", selection.mode))
+    {
+        table.out <- First(table, keep = selections,
+                           unit = unit, calendar = calendar, ...)
+        selections <- seq_len(nrow(table.out))
+    }else if (grepl("^last", selection.mode)){
+        table.out <- Last(table, keep = selections,
+                          unit = unit, calendar = calendar, ...)
+        selections <- (nrow(table) - nrow(table.out) + 1):nrow(table)
+    }else
+    {
+        if (is.character(selections))
+            selections <- checkSuppliedIndices(table, selections, 1)
 
-    if (hasColSpan(table.orig))
-         table <- updateTableColSpanAttribute(table, table.orig, idx)
-    table <- flipU::CopyAttributes(table, table.orig)
-    return(table)
+        if (n.dims == 1){
+            table.out <- table[selections, drop = FALSE]
+        }else if (n.dims == 2){
+            table.out <- table[selections, , drop = FALSE]
+        }else if (n.dims == 3){
+            table.out <- table[selections, , , drop = FALSE]
+        }
+
+    }
+    if (NROW(table.out) == 0L)
+        stop("No rows selected, output contains no rows.")
+
+    table.out <- flipU::CopyAttributes(table.out, table)
+    if (!is.null(attr(table, "span")) && !is.null(attr(table, "span")$rows))
+        table.out <- updateTableRowSpanAttribute(table.out, table, selections)
+    return(table.out)
+}
+
+selectFromColumns <- function(table, table.orig, selection.mode = "vector",
+                           selections = NULL, unit, calendar, ...)
+{
+    n.dims <- length(dim(table))
+    if(selection.mode == "range")
+        selections <- parseRangeString(selections)
+    else if (selection.mode == "date range")
+        selections <- findDatesInTable(table, selections, 2)
+
+    if (selection.mode == "first columns"){
+        table.out <- First(table, keep = selections, unit = "Column", ...)
+        selections <- seq_len(ncol(table.out))
+    }else if (selection.mode == "last columns"){
+        table.out <- Last(table, keep = selections, unit = "Column", ...)
+        selections <- (ncol(table) - ncol(table.out) + 1):ncol(table)
+    }else if (selection.mode == "first date-time periods"){
+        table.out <- t(First(t(table),  # t() in case date labels in rows and columns
+                             keep = selections, unit = unit,
+                             calendar = calendar, ...))
+        selections <- seq_len(ncol(table.out))
+    }else if (selection.mode == "last date-time periods"){
+        table.out <- t(Last(t(table),  # t() in case date labels in rows and columns
+                             keep = selections, unit = unit,
+                            calendar = calendar, ...))
+        selections <- (ncol(table) - ncol(table.out) + 1):ncol(table)
+    }else
+    {
+        if (is.character(selections))
+            selections <- checkSuppliedIndices(table, selections, 2)
+        if (length(dim(table)) == 2){
+            table.out <- table[, selections, drop = FALSE]
+        }else if (length(dim(table)) == 3)
+            table.out <- table[, selections, , drop = FALSE]
+    }
+    if (NCOL(table.out) == 0L)
+        stop("No columns selected, output contains no columns.")
+
+    table.out <- flipU::CopyAttributes(table.out, table)
+    if (hasColSpan(table))
+         table.out <- updateTableColSpanAttribute(table.out, table, selections)
+    return(table.out)
 }
 
 checkSuppliedIndices <- function(table, indices, dim)
@@ -74,15 +188,50 @@ checkSuppliedIndices <- function(table, indices, dim)
         dim.str <- ifelse(dim == 1, "row", "column")
         if (length(bad.idx) == length(names))
             stop("No selections found in the ", dim.str,
-                 " labels. If using a page Control, check ",
-                 " the item list for the Control and edit if necessary.")
+                 " labels. If using a page control, check ",
+                 " the item list for the control and edit if necessary.")
 
          bad.names <- paste(indices[bad.idx], collapse = ", ")
          warning("The following labels/selections were not found in the ",
                  dim.str, " labels of the table and will be ignored: ", bad.names,
-                 ". Please check the item list of the Control being used ",
-                 "  for making selections and edit if necessary.")
+                 ". Please check the item list of the control being used ",
+                 "for making selections and edit if necessary.")
         return(indices[-bad.idx])
     }
-    return(indicies)
+    return(indices)
+}
+
+#' @param range string specifying numeric ranges, e.g. \code{"1-3, 5-7,12"}
+#' @return An integer vector of values included in \code{range}.
+#' @noRd
+#' @importFrom flipU ConvertCommaSeparatedStringToVector
+#' @importFrom magrittr %>%
+parseRangeString <- function(range)
+{
+    indices <- range %>% ConvertCommaSeparatedStringToVector %>%
+        strsplit(split = " ?- ?") %>%
+        lapply(FUN = as.numeric) %>%
+        lapply(FUN = function(x) if (length(x) == 2 && !anyNA(x))
+                                     return(seq(x[1], x[2]))
+                                 else return(x)) %>%
+        unlist
+    if (anyNA(indices))
+        stop("The supplied range of indices could not be parsed. It must ",
+             "contain a comma-separated list of numbers; e.g., '1,3-5,8,10-15'.")
+    return(indices)
+}
+
+#' @importFrom flipTime AsDate
+findDatesInTable <- function(table, date.range, dim = 1)
+{
+    dates <- strsplit(date.range, "--")[[1L]]
+    start.date <- AsDate(dates[1])
+    end.date <- AsDate(dates[2])
+    if (dim == 1)
+        tnames <- rownames(table)
+    else
+        tnames <- colnames(table)
+
+    names.dates <- AsDateTime(tnames)
+    return(which(names.dates >= start.date & names.dates <= end.date))
 }
--- tests/testthat/test-select.R
+++ tests/testthat/test-select.R
@@ -91,3 +91,202 @@ test_that("5D QTables",
     expect_equal(colnames(qtable.5D.flat)[9], "Health-conscious: Diet Coke")
     expect_equal(dim(qtable.5D.flat), dim.target)
 })
+
+
+test_that("Q Tables with vector selection mode",
+{
+    idx <- 1:3
+    out <- SelectFromTable(qtable.2D.xtab,
+                    row.selection.mode = "vector",
+                    row.selections = idx)
+    expect_equivalent(out, qtable.2D.xtab[idx, ])
+    expect_equal(attr(out, "span")$rows,
+                 attr(qtable.2D.xtab, "span")$rows[idx, , drop = FALSE])
+
+    idx <- c(2, 5, 8)
+    out <- SelectFromTable(qtable.2D.xtab,
+                    column.selection.mode = "vector",
+                    column.selections = idx)
+    expect_equivalent(out, qtable.2D.xtab[, idx])
+    expect_equal(attr(out, "span")$columns,
+                 attr(qtable.2D.xtab, "span")$columns[idx, , drop = FALSE])
+
+    ridx <- c("18 to 24", "SUM")
+    expect_warning(out <- SelectFromTable(qtable.2D.xtab,
+                    row.selection.mode = "vector",
+                    row.selections = ridx), "ignored: SUM")
+    expect_equal(nrow(out), 1)
+    expect_equivalent(attr(out, "span")$rows, "18 to 24")
+
+    ridx <- c("Pepsi", "Diet Pepsi", "Pepsi Max")
+    cidx <- c("Older", "Open to new experiences")
+    out <- SelectFromTable(qtable.2D.multi.VS,
+                    row.selection.mode = "vector",
+                    row.selections = ridx,
+                    column.selections = cidx)
+    expect_equal(dim(out), c(length(ridx), length(cidx)))
+    expect_equal(rownames(out), ridx)
+    expect_equal(colnames(out), cidx)
+})
+
+test_that("Selection with higher dimensional Q Tables",
+{
+    qtable.flat <- FlattenTableAndDropStatisticsIfNecessary(qtable.4D.1stat)
+
+    ridx <- grep("at home", rownames(qtable.flat), value = TRUE)
+    cidx <- grep("[dD]islike", colnames(qtable.flat))
+    out <- SelectFromTable(qtable.4D.1stat, row.selections = ridx,
+                           column.selections = cidx)
+    expect_equivalent(out, qtable.flat[ridx, cidx])
+})
+
+test_that("Q Tables with Last and First rows",
+{
+    out <- SelectFromTable(qtable.2D.multi.VS,
+                    row.selection.mode = "First rows",
+                    row.selections = 3)
+    expect_equivalent(out, qtable.2D.multi.VS[1:3, ])
+    expect_equivalent(out, First(qtable.2D.multi.VS, 3))
+    expect_equal(attr(out, "statistic"), "%")
+
+    out <- SelectFromTable(qtable.2D.multi.VS,
+                    row.selection.mode = "Last rows",
+                    row.selections = 2)
+    n <- nrow(qtable.2D.multi.VS)
+    expect_equivalent(out, qtable.2D.multi.VS[(n-1):n, ])
+    expect_equivalent(out, Last(qtable.2D.multi.VS, 2))
+    expect_equal(attr(out, "questiontypes"), "PickAnyGrid")
+})
+
+test_that("Q Tables with Last and First columns",
+{
+    out <- SelectFromTable(qtable.2D.xtab,
+                    column.selection.mode = "first columns",
+                    column.selections = 3)
+    expect_equivalent(out, qtable.2D.xtab[, 1:3])
+    expect_equivalent(out, First(qtable.2D.xtab, 3, unit = "Column"))
+    expect_equal(attr(out, "statistic"), "Column %")
+
+    out <- SelectFromTable(qtable.2D.xtab,
+                    column.selection.mode = "last columns",
+                    column.selections = 1)
+    n <- ncol(qtable.2D.xtab)
+    expect_equivalent(out, qtable.2D.xtab[, n])
+    expect_equivalent(out, Last(qtable.2D.xtab, 1, unit = "Column"))
+    expect_equal(colnames(out), "$200,001 or more")
+
+
+    expect_warning(out <- SelectFromTable(qtable.3D.nominal.multi.multistat,
+                           row.selection.mode = "Last rows",
+                           row.selections = 5,
+                           column.selection.mode = "First columns",
+                           column.selections = 2), "Row %")
+    n <- nrow(qtable.3D.nominal.multi.multistat)
+    expect_equivalent(out, qtable.3D.nominal.multi.multistat[(n-4):n, 1:2, 1])
+    expect_equivalent(attr(out, "span")$rows,
+                      attr(qtable.3D.nominal.multi.multistat,
+                           "span")$rows[(n-4):n, , drop = FALSE])
+})
+
+test_that("last and first date selection mode",
+{
+    dates <- seq(as.Date("2000/1/15"), as.Date("2001/01/15"), length.out = 14)
+    x <- matrix(seq_len(length(dates)*5), length(dates), 5)
+    colnames(x) <- LETTERS[1:ncol(x)]
+    date.fmt <- "%Y/%m/%d"
+    rownames(x) <- format(dates, date.fmt)
+    out <- SelectFromTable(x, row.selection.mode = "first date-time periods",
+                           row.selections = 1, unit = "Quarter", calendar = TRUE)
+    expect_equal(dim(out), c(3, ncol(x)))
+    expect_equivalent(out, First(x, keep = 1, unit = "Quarter"))
+    expect_equal(rownames(out), format(dates[1:3], date.fmt))
+
+    out <- SelectFromTable(x, row.selection.mode = "first date-time periods",
+                           row.selections = 1, unit = "Quarter", calendar = FALSE)
+    expect_equal(dim(out), c(4, ncol(x)))
+    expect_equal(out, First(x, keep = 1, unit = "Quarter", calendar = FALSE))
+
+    out <- SelectFromTable(x, row.selection.mode = "first date-time periods",
+                           row.selections = 1, unit = "Year", calendar = FALSE)
+    expect_equal(dim(out), c(nrow(x) - 1, ncol(x)))
+
+
+    out <- SelectFromTable(x, row.selection.mode = "last date-time periods",
+                           row.selections = 1, unit = "Year", calendar = FALSE)
+    expect_equal(out, Last(x, keep = 1, unit = "Year", calendar = FALSE))
+    out <- SelectFromTable(x, row.selection.mode = "last date-time periods",
+                           row.selections = 1, unit = "Year", calendar = TRUE)
+    expect_equal(dim(out), c(1, ncol(x)))
+
+    x <- t(x)
+    out <- SelectFromTable(x, column.selection.mode = "first date-time periods",
+                           column.selections = 3, unit = "Month")
+    expect_equal(dim(out), c(nrow(x), 3))
+    out <- SelectFromTable(x, column.selection.mode = "first date-time periods",
+                           column.selections = 3, unit = "Month", calendar = FALSE)
+    expect_equal(dim(out), c(nrow(x), 4))
+
+    out <- SelectFromTable(x, column.selection.mode = "last date-time periods",
+                           column.selections = 5, unit = "Week")
+    expect_equal(dim(out), c(nrow(x), 2))
+    expect_equal(out, Last(out, keep = 5, unit = "Week"))
+
+    row.keep <- c(2, 4)
+    out <- SelectFromTable(x, row.selection.mode = "vector",
+                           row.selections = LETTERS[row.keep],
+                           column.selection.mode = "last date-time periods",
+                           column.selections = 30, unit = "Day", calendar = FALSE)
+    expect_equal(dim(out), c(2, 2))
+    expect_equivalent(out, Last(x, keep = 30, unit = "Day",
+                                calendar = FALSE)[row.keep, ])
+})
+
+test_that("range selection mode",
+{
+    x <- matrix(0, 26, 26)
+    colnames(x) <- LETTERS
+    rownames(x) <- letters
+    out <- SelectFromTable(x, row.selection.mode = "range",
+                           row.selections = "1,3,24-26")
+    expect_equal(dim(out), c(5, 26))
+    expect_equal(rownames(out), c("a", "c", "x", "y", "z"))
+
+
+    out <- SelectFromTable(x, column.selection.mode = "range",
+                           column.selections = "5-8")
+    expect_equal(dim(out), c(26, 4))
+    expect_equal(colnames(out), c("E", "F", "G", "H"))
+
+    out <- SelectFromTable(x, row.selection.mode = "range",
+                           row.selections = "1-3,21",
+                           column.selection.mode = "range",
+                           column.selections = "13")
+    expect_equal(dim(out), c(4, 1))
+    expect_equal(colnames(out), "M")
+    expect_equal(rownames(out), letters[c(1:3, 21)])
+})
+
+test_that("date range selection mode",
+{
+    dates <- seq(as.Date("2000/1/1"), as.Date("2003/1/1"), by = "quarter")
+    x <- matrix(seq_len(65), 13, 5)
+    colnames(x) <- LETTERS[1:ncol(x)]
+    rownames(x) <- format(dates, "%Y-%m-%d")
+    out <- SelectFromTable(x, row.selection.mode = "date range",
+                           row.selections = "2000-01-01--2000-12-31")
+    expect_equal(dim(out), c(4, ncol(x)))
+    expect_equivalent(out, First(x, keep = 1, unit = "Year"))
+    expect_equivalent(out, First(x, keep = 4, unit = "Quarter"))
+    expect_equal(rownames(out), format(dates[1:4], "%Y-%m-%d"))
+
+    dates <- seq(as.Date("2000/1/6"), as.Date("2001/1/15"), length.out = 13)
+    x <- matrix(seq_len(52), 4, 13)
+    rownames(x) <- LETTERS[1:nrow(x)]
+    date.fmt <- "%d/%m/%Y"
+    colnames(x) <- format(dates, date.fmt)
+    out <- SelectFromTable(x, column.selection.mode = "date range",
+                           column.selections = "01/15/2000--11/15/2000")
+    ncol.expect <- sum(dates >= as.Date("2000/01/15") & dates <= as.Date("2000/11/15"))
+    expect_equal(dim(out), c(4, ncol.expect))
+    expect_equal(colnames(out), format(dates[2:11], date.fmt))
+})
