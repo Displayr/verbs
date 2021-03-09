0b0707a27e2b853da717bd9042d1c2ad6cf60d79
jrwishart <justin.wishart@displayr.com>
2021-03-09 16:48:41 +1100

    DS-3256 Add vector tests for Divide [revdep skip]

--- tests/testthat/test-divide.R
+++ tests/testthat/test-divide.R
@@ -57,8 +57,40 @@ test_that("Divide: vector inputs", {
     inputs <- replicate(n,
                         replicate(2, runif(m, min = -1e3, max = 1e3), simplify = FALSE),
                         simplify = FALSE)
-    expected.outputs <- lapply(inputs, function(x) array(x[[1L]]/x[[2L]],
-                                                         dim = length(x[[1L]])))
+    expected.outputs <- lapply(inputs, function(x) x[[1L]]/x[[2L]])
     # Basic unnamed usage tests
     invisible(mapply(checkDivideOutput, inputs, expected.outputs))
+    # Basic named vector matching
+    numerator   <- c(a = 10, b = 9, c = 8, d = 7, e = 6)
+    denominator <- c(c = 1,  e = 2, b = 3, a = 4, d = 5)
+    expected.output <- numerator/denominator[names(numerator)]
+    checkDivideOutput(list(numerator, denominator), expected.output)
+    # Named vector, different size
+    numerator   <- c(a = 10, b = 9, c = 8, d = 7, e = 6, f = 5)
+    denominator <- c(c = 1,  e = 2, b = 3, a = 4, d = 5)
+    matched.denominator <- c(denominator[names(numerator)[1:5]], f = 0)
+    expected.output <- numerator/matched.denominator
+    checkDivideOutput(list(numerator, denominator), expected.output)
+    # Same named vector, different size. Sign switched and keep missing
+    numerator   <- - c(a = 10, b = 9, c = 8, d = 7, e = 6, f = 5)
+    denominator <-   c(c = 1,  e = 2, b = 3, a = 4, d = 5)
+    matched.denominator <- c(denominator[names(numerator)[1:5]], f = NA)
+    expected.output <- numerator/matched.denominator
+    checkDivideOutput(list(numerator, denominator), expected.output, remove.missing = FALSE)
+    # Named vector, but jumbled order in numerator, denominator longer
+    numerator   <- c(c = 1,  a = 2, b = 3, e = 4, d = 5)
+    denominator <- c(a = 10, b = 9, c = 8, d = 7, e = 6, f = 5)
+    matched.denominator <- c(denominator[names(numerator)], f = 5)
+    matched.numerator   <- c(numerator, f = 0)
+    expected.output <- matched.numerator/matched.denominator
+    checkDivideOutput(list(numerator, denominator), expected.output)
+    # Numerator vector, denominator scalar
+    numerator <- c(a = 10, b = 9, c = 8, d = 7, e = 6)
+    denominator <- 5
+    reshaped.denominator <- rep(5, length(numerator))
+    expected.output <- numerator/reshaped.denominator
+    checkDivideOutput(list(numerator, denominator), expected.output, match.rows = "No")
+    expect_warning(checkDivideOutput(list(numerator, denominator), expected.output,
+                                     match.rows = "No", warn = TRUE),
+                   "A scalar element was reshaped to a vector with 5 rows")
 })
