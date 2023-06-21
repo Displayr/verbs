98bc24f7126a8e2da915606c71a8d5b173a2e1b9
chrisfacer <chris.facer@q-researchsoftware.com>
2023-06-21 11:52:03 +1000

    Update copyAttributesIfNotQTable

--- R/utilities.R
+++ R/utilities.R
@@ -1979,6 +1979,6 @@ ValidateFilterForEachColumnVariants <- function(data, filter) {
 #' @noRd
 copyAttributesIfNotQTable <- function(x, y)
 {
-    if (inherits(y, "QTable")) return(x)
+    if (inherits(y, "qTable")) return(x)
     CopyAttributes(x, y)
 }
