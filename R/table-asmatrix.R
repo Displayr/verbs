4e0e48981c8334f8d382a65404cf43e1756523a7
bwinsley <brandon.winsley@displayr.com>
2024-05-10 11:57:30 +1000

    Add table-asmatrix function

---
+++ R/table-asmatrix.R
@@ -0,0 +1,27 @@
+#' @export
+t.QTable <- function(x)
+{
+    if (checkIsQTable(x)) {
+        all_attrs <- attributes(x)
+        attrs_to_keep <- c("dim", "dimnames")
+        attrs_to_remove <- setdiff(names(all_attrs), attrs_to_keep)
+
+        x_cleaned <- x
+        for (attr_name in attrs_to_remove) {
+            x_cleaned <- attributes(x_cleaned)[attr_name] <- NULL
+        }
+        x_cleaned
+    } else {
+        stop('Input must be a QTable object')
+    }
+}
+
+checkIsQTable <- function(x) {
+    # Check its classes
+    class_x <- class(x)
+    is_qtable <- "QTable" %in% class_x
+    valid_classes_only <- length(setdiff(class_x, c("QTable", "matrix", "array"))) == 0
+
+    is_qtable && valid_classes_only
+}
+
