f4f05e2e8d8528b4cb43f0b1d6fae3a2c496f419
bwinsley <brandon.winsley@displayr.com>
2024-05-10 15:36:30 +1000

    Add more tests

--- tests/testthat/test-asarray.R
+++ tests/testthat/test-asarray.R
@@ -1,5 +1,26 @@
 context("AsArray")
 
+testthat("Column array", {
+    x <- structure(
+        c(1:3), 
+        foo = "bar"
+    )
+    expect_equal(AsArray(x), c(1:3))
+})
+
+testthat("1D Array", {
+    x <- structure(
+        array(1:3, dim = c(1)), 
+        dimnames = list("a"), 
+        foo = "bar"
+    )
+    exp <- structure(
+        array(1:3, dim = c(1)), 
+        dimnames = list("a")
+    )
+    expect_equal(AsArray(x), exp)
+})
+
 testthat("Tables", {
     x <- structure(
         matrix(1:12, nrow = 3), 
@@ -9,6 +30,45 @@ testthat("Tables", {
     exp <- structure(
         matrix(1:12, nrow = 3), 
         dimnames = list(letters[1:3], LETTERS[1:4])
+    )
+    expect_equal(AsArray(x), exp)
+})
+
+testthat("3D Array", {
+    x <- structure(
+        array(1:24, dim = c(2, 3, 4)), 
+        dimnames = list(letters[1:2], LETTERS[1:3], letters[1:4]), 
+        foo = "bar"
+    )
+    exp <- structure(
+        array(1:24, dim = c(2, 3, 4)), 
+        dimnames = list(letters[1:2], LETTERS[1:3], letters[1:4])
+    )
+    expect_equal(AsArray(x), exp)
+})
+
+testthat("4D Array", {
+    x <- structure(
+        array(1:16, dim = c(2, 2, 2, 2)), 
+        dimnames = list(letters[1:2], LETTERS[1:2], letters[1:2], LETTERS[1:2]), 
+        foo = "bar"
+    )
+    exp <- structure(
+        array(1:16, dim = c(2, 2, 2, 2)), 
+        dimnames = list(letters[1:2], LETTERS[1:2], letters[1:2], LETTERS[1:2])
+    )
+    expect_equal(AsArray(x), exp)
+})
+
+testthat("5D Array", {
+    x <- structure(
+        array(1:16, dim = c(2, 2, 2, 2, 2)), 
+        dimnames = list(letters[1:2], LETTERS[1:2], letters[1:2], LETTERS[1:2], letters[1:2]), 
+        foo = "bar"
+    )
+    exp <- structure(
+        array(1:16, dim = c(2, 2, 2, 2, 2)), 
+        dimnames = list(letters[1:2], LETTERS[1:2], letters[1:2], LETTERS[1:2], letters[1:2])
     )
     expect_equal(AsArray(x), exp)
 })
