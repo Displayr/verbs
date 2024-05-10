98b384f5e3523e5b8b96eda556216dd4e47c5af1
bwinsley <brandon.winsley@displayr.com>
2024-05-10 14:42:22 +1000

    Optimise function

--- R/asarray.R
+++ R/asarray.R
@@ -1,14 +1,28 @@
+#' Convert objects to arrays
+#'
+#' @param x Any object
+#' @return The same object with all attributes removed except for dim and dimnames
 #' @export
 AsArray <- function(x)
 {
-    all_attrs <- attributes(x)
-    attrs_to_keep <- c("dim", "dimnames")
-    attrs_to_remove <- setdiff(names(all_attrs), attrs_to_keep)
+    n.dims <- length(dim(x))
+    if (n.dims < 5 && n.dims > 0) return(useBaseSubscript(x))
+    all.attrs <- attributes(x)
+    attrs.to.keep <- c("dim", "dimnames")
+    attrs.to.remove <- setdiff(names(all.attrs), attrs.to.keep)
 
-    x_cleaned <- x
-    for (attr_name in attrs_to_remove) {
-        attributes(x_cleaned)[attr_name] <- NULL
-    }
-    x_cleaned
+    attributes(x)[attrs.to.remove] <- NULL
+    x
 }
 
+useBaseSubscript <- function(x) {
+    y <- unclass(x)
+    n.dims <- length(dim(x))
+    switch(n.dims,
+        y[TRUE],
+        y[, ],
+        y[, , ],
+        y[, , , ],
+        y[, , , , ]
+    )
+}
