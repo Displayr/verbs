ea50ba4ec74d8b7c693580a94da2cd9aaa53320f
Bruce Tan <bruce.tan@displayr.com>
2025-03-11 17:19:42 +1300

    fix format

--- package.nix
+++ package.nix
@@ -4,12 +4,12 @@ pkgs.rPackages.buildRPackage {
   name = "verbs";
   version = displayrUtils.extractRVersion (builtins.readFile ./DESCRIPTION); 
   src = ./.;
-  description = "A collection of operations/actions/"verbs" providing an intuitive interface for many common operations (e.g. Sum, First, Average, Maximum, ...) on various inputs/data structures. Created to simplify common user actions in Displayr with a wide variety of inputs.";
+  description = ''A collection of operations/actions/"verbs" providing an intuitive interface for many common operations (e.g. Sum, First, Average, Maximum, ...) on various inputs/data structures. Created to simplify common user actions in Displayr with a wide variety of inputs.'';
   propagatedBuildInputs = with pkgs.rPackages; [ 
-    flipTime
     flipU
     flipFormat
-    lubridate
+    flipTime
     flipTransformations
+    lubridate
   ];
 }
